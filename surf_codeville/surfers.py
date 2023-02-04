ffb15cd0859aee4c7fab3551fa680a73bdf06906
Luiz Antunes <luizrantunes@outlook.com>
2023-02-03 22:47:07 -0300

    learning - 3/2/23

--- surf_codeville/surfers.py
+++ surf_codeville/surfers.py
@@ -4,5 +4,7 @@ if velocidadeMotorista > 110:
     print("Você tá muito rápido!")
 elif velocidadeMotorista > 40 and velocidadeMotorista <= 110:
     print("Tá dentro do limite de velocidade!")
-else:
+elif velocidadeMotorista < 40:
     print("Cê tá indo devagar demais")
+else:
+    print("Null")
