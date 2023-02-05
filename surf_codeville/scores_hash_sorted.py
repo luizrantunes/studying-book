250c3d2937fe227a1d0898c9ada59fdcbe1ec465
Luiz Antunes <102562859+luizrantunes@users.noreply.github.com>
2023-02-04 22:52:48 -0300

    Delete surf_codeville directory

--- surf_codeville/scores_hash_sorted.py
+++
@@ -1,9 +0,0 @@
-scores = {}
-result_f = open("results.txt")
-for line in result_f:
-    (name, score) = line.split()
-    scores[score] = name
-result_f.close()
-print("The top scores were:")
-for each_score in sorted(scores.keys(), reverse = True):
-    print('O surfista ' + scores[each_score] + ' marcou ' + each_score + ' pontos.')
